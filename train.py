36e18e06f86c1e69fc7879d5023b4bfdc7adfe62
tuanying <2659505964@qq.com>
2025-10-21 14:48:45 +0800

    train.py

--- train.py
+++ train.py
@@ -2,14 +2,13 @@ from ultralytics.models import YOLO
 
 
 if __name__ == '__main__':
-    model = YOLO(r'/mnt/workspace/01yolo11n/ultralytics/cfg/models/11/yolo11n.yaml')  # 此处以 m 为例，只需写yolov11m即可定位到m模型
-    model.train(data=r'/mnt/workspace/pv6/data.yaml',
+    model = YOLO(r'/mnt/workspace/SHS-YOLO/ultralytics/cfg/models/11/SHS-YOLO.yaml')  # 此处以 m 为例，只需写yolov11m即可定位到m模型
+    model.train(data=r'/mnt/workspace/pv2/data.yaml',
                 imgsz=640,
                 epochs=200,
                 single_cls=False,  # 多类别设置False
                 batch=64,
                 workers=8,
-                device='0',
-                project=r'runs/detect/yolo11n_imp/1111/'
+                device='0'
                 )
                 
