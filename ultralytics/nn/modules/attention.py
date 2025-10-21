7f30b3fd529ce85d1f71a8a43401ae04caca5e83
UltralyticsAssistant <web@ultralytics.com>
2025-10-21 06:31:13 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/modules/attention.py
+++ ultralytics/nn/modules/attention.py
@@ -1,51 +1,69 @@
-import typing as t
-
 import torch
 import torch.nn as nn
 from einops import rearrange
 from mmengine.model import BaseModule
-__all__ = ['SCSA']
+
+__all__ = ["SCSA"]
 
 
 class SCSA(BaseModule):
-
     def __init__(
-            self,
-            dim: int,
-            head_num: int,
-            window_size: int = 7,
-            group_kernel_sizes: t.List[int] = [3, 5, 7, 9],
-            qkv_bias: bool = False,
-            fuse_bn: bool = False,
-            norm_cfg: t.Dict = dict(type='BN'),
-            act_cfg: t.Dict = dict(type='ReLU'),
-            down_sample_mode: str = 'avg_pool',
-            attn_drop_ratio: float = 0.,
-            gate_layer: str = 'sigmoid',
+        self,
+        dim: int,
+        head_num: int,
+        window_size: int = 7,
+        group_kernel_sizes: list[int] = [3, 5, 7, 9],
+        qkv_bias: bool = False,
+        fuse_bn: bool = False,
+        norm_cfg: dict = dict(type="BN"),
+        act_cfg: dict = dict(type="ReLU"),
+        down_sample_mode: str = "avg_pool",
+        attn_drop_ratio: float = 0.0,
+        gate_layer: str = "sigmoid",
     ):
-        super(SCSA, self).__init__()
+        super().__init__()
         self.dim = dim
         self.head_num = head_num
         self.head_dim = dim // head_num
-        self.scaler = self.head_dim ** -0.5
+        self.scaler = self.head_dim**-0.5
         self.group_kernel_sizes = group_kernel_sizes
         self.window_size = window_size
         self.qkv_bias = qkv_bias
         self.fuse_bn = fuse_bn
         self.down_sample_mode = down_sample_mode
 
-        assert self.dim // 4, 'The dimension of input feature should be divisible by 4.'
+        assert self.dim // 4, "The dimension of input feature should be divisible by 4."
         self.group_chans = group_chans = self.dim // 4
 
-        self.local_dwc = nn.Conv1d(group_chans, group_chans, kernel_size=group_kernel_sizes[0],
-                                   padding=group_kernel_sizes[0] // 2, groups=group_chans)
-        self.global_dwc_s = nn.Conv1d(group_chans, group_chans, kernel_size=group_kernel_sizes[1],
-                                      padding=group_kernel_sizes[1] // 2, groups=group_chans)
-        self.global_dwc_m = nn.Conv1d(group_chans, group_chans, kernel_size=group_kernel_sizes[2],
-                                      padding=group_kernel_sizes[2] // 2, groups=group_chans)
-        self.global_dwc_l = nn.Conv1d(group_chans, group_chans, kernel_size=group_kernel_sizes[3],
-                                      padding=group_kernel_sizes[3] // 2, groups=group_chans)
-        self.sa_gate = nn.Softmax(dim=2) if gate_layer == 'softmax' else nn.Sigmoid()
+        self.local_dwc = nn.Conv1d(
+            group_chans,
+            group_chans,
+            kernel_size=group_kernel_sizes[0],
+            padding=group_kernel_sizes[0] // 2,
+            groups=group_chans,
+        )
+        self.global_dwc_s = nn.Conv1d(
+            group_chans,
+            group_chans,
+            kernel_size=group_kernel_sizes[1],
+            padding=group_kernel_sizes[1] // 2,
+            groups=group_chans,
+        )
+        self.global_dwc_m = nn.Conv1d(
+            group_chans,
+            group_chans,
+            kernel_size=group_kernel_sizes[2],
+            padding=group_kernel_sizes[2] // 2,
+            groups=group_chans,
+        )
+        self.global_dwc_l = nn.Conv1d(
+            group_chans,
+            group_chans,
+            kernel_size=group_kernel_sizes[3],
+            padding=group_kernel_sizes[3] // 2,
+            groups=group_chans,
+        )
+        self.sa_gate = nn.Softmax(dim=2) if gate_layer == "softmax" else nn.Sigmoid()
         self.norm_h = nn.GroupNorm(4, dim)
         self.norm_w = nn.GroupNorm(4, dim)
 
@@ -55,24 +73,22 @@ class SCSA(BaseModule):
         self.k = nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=1, bias=qkv_bias, groups=dim)
         self.v = nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=1, bias=qkv_bias, groups=dim)
         self.attn_drop = nn.Dropout(attn_drop_ratio)
-        self.ca_gate = nn.Softmax(dim=1) if gate_layer == 'softmax' else nn.Sigmoid()
+        self.ca_gate = nn.Softmax(dim=1) if gate_layer == "softmax" else nn.Sigmoid()
 
         if window_size == -1:
             self.down_func = nn.AdaptiveAvgPool2d((1, 1))
         else:
-            if down_sample_mode == 'recombination':
+            if down_sample_mode == "recombination":
                 self.down_func = self.space_to_chans
                 # dimensionality reduction
-                self.conv_d = nn.Conv2d(in_channels=dim * window_size ** 2, out_channels=dim, kernel_size=1, bias=False)
-            elif down_sample_mode == 'avg_pool':
+                self.conv_d = nn.Conv2d(in_channels=dim * window_size**2, out_channels=dim, kernel_size=1, bias=False)
+            elif down_sample_mode == "avg_pool":
                 self.down_func = nn.AvgPool2d(kernel_size=(window_size, window_size), stride=window_size)
-            elif down_sample_mode == 'max_pool':
+            elif down_sample_mode == "max_pool":
                 self.down_func = nn.MaxPool2d(kernel_size=(window_size, window_size), stride=window_size)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        The dim of x is (B, C, H, W)
-        """
+        """The dim of x is (B, C, H, W)."""
         # Spatial attention priority calculation
         b, c, h_, w_ = x.size()
         # (B, C, H)
@@ -82,20 +98,34 @@ class SCSA(BaseModule):
         x_w = x.mean(dim=2)
         l_x_w, g_x_w_s, g_x_w_m, g_x_w_l = torch.split(x_w, self.group_chans, dim=1)
 
-        x_h_attn = self.sa_gate(self.norm_h(torch.cat((
-            self.local_dwc(l_x_h),
-            self.global_dwc_s(g_x_h_s),
-            self.global_dwc_m(g_x_h_m),
-            self.global_dwc_l(g_x_h_l),
-        ), dim=1)))
+        x_h_attn = self.sa_gate(
+            self.norm_h(
+                torch.cat(
+                    (
+                        self.local_dwc(l_x_h),
+                        self.global_dwc_s(g_x_h_s),
+                        self.global_dwc_m(g_x_h_m),
+                        self.global_dwc_l(g_x_h_l),
+                    ),
+                    dim=1,
+                )
+            )
+        )
         x_h_attn = x_h_attn.view(b, c, h_, 1)
 
-        x_w_attn = self.sa_gate(self.norm_w(torch.cat((
-            self.local_dwc(l_x_w),
-            self.global_dwc_s(g_x_w_s),
-            self.global_dwc_m(g_x_w_m),
-            self.global_dwc_l(g_x_w_l)
-        ), dim=1)))
+        x_w_attn = self.sa_gate(
+            self.norm_w(
+                torch.cat(
+                    (
+                        self.local_dwc(l_x_w),
+                        self.global_dwc_s(g_x_w_s),
+                        self.global_dwc_m(g_x_w_m),
+                        self.global_dwc_l(g_x_w_l),
+                    ),
+                    dim=1,
+                )
+            )
+        )
         x_w_attn = x_w_attn.view(b, c, 1, w_)
 
         x = x * x_h_attn * x_w_attn
@@ -112,12 +142,24 @@ class SCSA(BaseModule):
         k = self.k(y)
         v = self.v(y)
         # (B, C, H, W) -> (B, head_num, head_dim, N)
-        q = rearrange(q, 'b (head_num head_dim) h w -> b head_num head_dim (h w)', head_num=int(self.head_num),
-                      head_dim=int(self.head_dim))
-        k = rearrange(k, 'b (head_num head_dim) h w -> b head_num head_dim (h w)', head_num=int(self.head_num),
-                      head_dim=int(self.head_dim))
-        v = rearrange(v, 'b (head_num head_dim) h w -> b head_num head_dim (h w)', head_num=int(self.head_num),
-                      head_dim=int(self.head_dim))
+        q = rearrange(
+            q,
+            "b (head_num head_dim) h w -> b head_num head_dim (h w)",
+            head_num=int(self.head_num),
+            head_dim=int(self.head_dim),
+        )
+        k = rearrange(
+            k,
+            "b (head_num head_dim) h w -> b head_num head_dim (h w)",
+            head_num=int(self.head_num),
+            head_dim=int(self.head_dim),
+        )
+        v = rearrange(
+            v,
+            "b (head_num head_dim) h w -> b head_num head_dim (h w)",
+            head_num=int(self.head_num),
+            head_dim=int(self.head_dim),
+        )
 
         # (B, head_num, head_dim, head_dim)
         attn = q @ k.transpose(-2, -1) * self.scaler
@@ -125,19 +167,19 @@ class SCSA(BaseModule):
         # (B, head_num, head_dim, N)
         attn = attn @ v
         # (B, C, H_, W_)
-        attn = rearrange(attn, 'b head_num head_dim (h w) -> b (head_num head_dim) h w', h=int(h_), w=int(w_))
+        attn = rearrange(attn, "b head_num head_dim (h w) -> b (head_num head_dim) h w", h=int(h_), w=int(w_))
         # (B, C, 1, 1)
         attn = attn.mean((2, 3), keepdim=True)
         attn = self.ca_gate(attn)
         return attn * x
 
-if __name__ =='__main__':
 
-    scsa = SCSA(256,4)
-    #创建一个输入张量
+if __name__ == "__main__":
+    scsa = SCSA(256, 4)
+    # 创建一个输入张量
     batch_size = 8
-    input_tensor=torch.randn(batch_size, 256, 64, 64 )
-    #运行模型并打印输入和输出的形状
-    output_tensor =scsa(input_tensor)
-    print("Input shape:",input_tensor.shape)
-    print("0utput shape:",output_tensor.shape)
+    input_tensor = torch.randn(batch_size, 256, 64, 64)
+    # 运行模型并打印输入和输出的形状
+    output_tensor = scsa(input_tensor)
+    print("Input shape:", input_tensor.shape)
+    print("0utput shape:", output_tensor.shape)
